7c46f9cb7b4eb3d444562501c66d15b763d34a5e
TBernaus <tomasbernaussole@gmail.com>
2024-09-19 17:12:37 +0200

    test

--- scripts/api.js
+++ scripts/api.js
@@ -2,16 +2,16 @@
 const apiUrl = "../api.json";
 export let cardsData = [];
 
-// function cleanFilename(filename) {
-//   return filename.replace(/[\\/*?:"<>|]/g, '_').replace(/\s+/g, '_');
-// }
-
 function cleanFilename(filename) {
-  return filename
-    .toLowerCase()
-    .replace(/[\\/*?:"<>|]/g, '_')
-    .replace(/\s+/g, '_');
+  return filename.replace(/[\\/*?:"<>|]/g, '_').replace(/\s+/g, '_');
 }
+
+// function cleanFilename(filename) {
+//   return filename
+//     .toLowerCase()
+//     .replace(/[\\/*?:"<>|]/g, '_')
+//     .replace(/\s+/g, '_');
+// }
 
 export function fetchCardsData() {
   return fetch(apiUrl)
