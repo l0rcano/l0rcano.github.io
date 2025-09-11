116149c1d9e43892758b5802b37bfcac08358571
TBernaus <tomasbernaussole@gmail.com>
2025-09-11 14:28:11 +0200

    test

--- scripts/lang.js
+++ scripts/lang.js
@@ -8,6 +8,9 @@ export function getIdioma() {
   return idioma;
 }
 
+// export function updateApiUrl() {
+//   return `../api/api${idioma}.json`;
+// }
 export function updateApiUrl() {
-  return `../api/api${idioma}.json`;
+  return `https://lorcanajson.org/files/current/${idioma.toLowerCase()}/allCards.json`;
 }
